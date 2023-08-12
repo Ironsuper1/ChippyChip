bff4098d3c6378d889e455ce0e1243761e886d57
drumil <drumil.sevak@gmail.com>
2023-08-12 21:39:36 +1000

    Added PC and image

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 #### Fix fully selected design issue so a json netlist can be generated
 #### Use something like this: https://github.com/nturley/netlistsvg to generate an SVG based on netlist
 
+![Alt text](gpu_modules2.png)
--- src/processor.v
+++ src/processor.v
@@ -1,10 +1,25 @@
 module processor;
 
-  wire clk, rst;
+  wire clk, rst, en;
   reg [31:0] instruction = "11";
   reg [15:0] value_in = "12";
   reg [4:0] result;
 
+  prog_count p1 (clk, rst, en);
   assign result = instruction[5:1] & value_in[5:1];
+
+endmodule
+
+
+module prog_count;
+  parameter n = 32;
+  input clk, rst, en;
+  reg [n-1:0] Q;
+
+  always @ (posedge rst, posedge clk)
+    if (rst)
+      Q <= 0;
+    else if (en)
+      Q <= Q + 1;
 
 endmodule
